ebf7f6b0d459cb7b059fb19c96be83d1dbe6ef55
Yash Kaushal <arecus2212@gmail.com>
2026-02-06 17:57:23 +0530

    Fix Redis TLS

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,3 +1,13 @@
 import Redis from "ioredis";
 
-export const redis = new Redis();
+export const redis = new Redis(process.env.REDIS_URL!, {
+  tls: {}
+});
+
+redis.on("connect", () => {
+  console.log("Redis connected ✅");
+});
+
+redis.on("error", (err) => {
+  console.error("Redis error ❌", err);
+});
